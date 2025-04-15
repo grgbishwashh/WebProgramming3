e34f22fcd6ff84738c0efcfd31ef8ec951614e1d
grgbishwashh <grgbishwashh@gmail.com>
2025-04-15 11:35:33 +0900

    change on 4.15.11.35

--- src/Main.kt
+++ src/Main.kt
@@ -4,12 +4,16 @@ fun main() {
 
     println("Enter your name")
     val yourWord = readln()
+    println("Your name is $yourWord")
+
     println("Enter your age")
     val yourAge = readln()
-
-
-    println("Your name is $yourWord")
     println("Your age is $yourAge")
+
+    println("Enter your gender")
+    val gender = readln()
+    println("Your gender is $gender")
+
 }
 
 
